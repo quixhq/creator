cd7cd3bb8a56b6e10e81b51dc056d3160841f726
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-06-09 12:46:58 +0530

    responsive view

--- src/components/quiz/Option.tsx
+++ src/components/quiz/Option.tsx
@@ -1,7 +1,7 @@
 const Option = ({ option, number }: { option: string; number: string }) => {
   return (
-    <div className="px-6 py-4 rounded-lg border-2 border-blue/40 text-blue font-bold flex items-center">
-      <span className="mr-4 rounded-full h-12 w-12 flex items-center justify-center bg-blue/20">
+    <div className="px-2 lg:px-6 py-2 lg:py-4 rounded-lg border-2 border-blue/40 text-blue font-bold flex items-center">
+      <span className="mr-4 rounded-full h-8 lg:h-12 w-8 lg:w-12 flex items-center justify-center bg-blue/20">
         {number}
       </span>
       {option}
